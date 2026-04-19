0bc8993663351fe7b1fdee8125e0daec969d90b6
kovidh <immadisettykovidh08@gmail.com>
2026-04-19 19:53:36 +0530

    final

--- q5/q5.s
+++ q5/q5.s
@@ -8,9 +8,18 @@ c1: .space 1
 c2: .space 1
 
 .section .text
-.global _start
+.global main
 
-_start:
+main:
+    # save ra and s0-s4 (5 regs + ra = 6 * 8 = 48 bytes, round to 48)
+    addi  sp, sp, -48
+    sd    ra,  0(sp)
+    sd    s0,  8(sp)
+    sd    s1, 16(sp)
+    sd    s2, 24(sp)
+    sd    s3, 32(sp)
+    sd    s4, 40(sp)
+
     # openat(AT_FDCWD, "input.txt", O_RDONLY, 0)
     li    a7, 56
     li    a0, -100
@@ -18,8 +27,8 @@ _start:
     li    a2, 0
     li    a3, 0
     ecall
-    mv    s0, a0
-    bltz  s0, exit
+    mv    s0, a0            # s0 = fd
+    bltz  s0, print_no      # open failed
 
     # lseek(fd, 0, SEEK_END) -> file size in s1
     li    a7, 62
@@ -27,19 +36,17 @@ _start:
     li    a1, 0
     li    a2, 2
     ecall
-    mv    s1, a0
+    mv    s1, a0            # s1 = size
 
-    # empty file is a palindrome
-    blez  s1, print_yes
+    blez  s1, print_yes     # empty file is a palindrome
 
-    # left = 0, right = size - 1
-    li    s2, 0
-    addi  s3, s1, -1
+    li    s2, 0             # s2 = left index
+    addi  s3, s1, -1        # s3 = right index
 
 loop:
     bge   s2, s3, print_yes
 
-    # read char at left index
+    # seek to left, read one byte into c1
     li    a7, 62
     mv    a0, s0
     mv    a1, s2
@@ -51,7 +58,7 @@ loop:
     li    a2, 1
     ecall
 
-    # read char at right index
+    # seek to right, read one byte into c2
     li    a7, 62
     mv    a0, s0
     mv    a1, s3
@@ -63,7 +70,7 @@ loop:
     li    a2, 1
     ecall
 
-    # compare
+    # compare c1 and c2
     la    t0, c1
     lb    t1, 0(t0)
     la    t2, c2
@@ -75,22 +82,21 @@ loop:
     j     loop
 
 print_yes:
-    li    a7, 64
-    li    a0, 1
-    la    a1, yes_msg
-    li    a2, 4
-    ecall
-    j     exit
+    la    a0, yes_msg
+    call  printf
+    j     done
 
 print_no:
-    li    a7, 64
-    li    a0, 1
-    la    a1, no_msg
-    li    a2, 3
-    ecall
+    la    a0, no_msg
+    call  printf
 
-exit:
-    li    a7, 93
+done:
+    ld    ra,  0(sp)
+    ld    s0,  8(sp)
+    ld    s1, 16(sp)
+    ld    s2, 24(sp)
+    ld    s3, 32(sp)
+    ld    s4, 40(sp)
+    addi  sp, sp, 48
     li    a0, 0
-    ecall
-
+    ret
